ec284bd62e69c8bb0a841020fe6f0c42f7ce6d39
karthik enumarthi <kartheekenumarthi@gmail.com>
2019-11-16 01:25:15 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,34 @@
+function fun1()
+{
+    document.getElementById("cartitem1").style.display="flex";
+    document.getElementById("add1").style.display="none";
+    document.getElementById("remove1").style.display="initial";
+
+}
+function fun2()
+{
+    document.getElementById("cartitem2").style.display="flex";
+    document.getElementById("add2").style.display="none";
+    document.getElementById("remove2").style.display="initial";
+
+}
+function fun3()
+{
+    document.getElementById("cartitem1").style.display="none";
+    document.getElementById("add1").style.display="initial";
+    document.getElementById("remove1").style.display="none";
+    
+
+}
+function fun4()
+{
+    document.getElementById("cartitem2").style.display="none";
+    document.getElementById("add2").style.display="initial";
+    document.getElementById("remove2").style.display="none";
+
+}
+
+
+
+
+
